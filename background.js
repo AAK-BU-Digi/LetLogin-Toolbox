39f5e8062cbd2222a8f793d6eb635ae365a99984
sorentorp <soren.torp@gmail.com>
2021-04-07 13:02:49 +0200

    Update background.js

--- background.js
+++ background.js
@@ -2,34 +2,34 @@
 
 // handle Youtube cookies. 
 // When using Youtube without permission to log in, playing videoes can sometimes cause problems.
-
 var requestMade = false;
 
 chrome.webRequest.onBeforeRequest.addListener(
-  function (youtube) {
-    if (!requestMade) {
-      const url = new URL(youtube.url);
+  function (request) {
+   
+      const url = new URL(request.url);
 
-      chrome.cookies.getAll({ url: url.origin }, function (cookies) {
+ //     chrome.cookies.getAll({ url: url.origin }, function (cookies) {
+   
         if (cookies.length === 0) {
-//            console.log(cookies + "deleted");
+            console.log(cookies + "deleted");
           return;
         } else {
           cookies.forEach((c) =>
+                          
+                          {              
+   //                       console.log("cookies " + c.name + "-value-" + c.value + "-path-" + c.path + "-domain-" + c.domain);
+              
             chrome.cookies.remove({
               name: c.name,
               url: url.origin,
               storeId: c.storeId,
             })
+          }
           );
         }
       });
 
-      requestMade = true;
-      setTimeout(() => {
-        requestMade = !requestMade;
-      }, 20000);
-    }
   },
-  { urls: ["*://www.youtube.com/*"] }
+  { urls: ["https://consent.youtube.com/*"] }
 );
